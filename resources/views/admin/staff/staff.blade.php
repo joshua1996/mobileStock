051e4ef02dfe5ace159cd8db2a32a6fae8ff3005
joshua1996 <kjw1996@hotmail.com>
2017-07-31 16:56:24 +0800

    staff problem

--- resources/views/admin/staff/staff.blade.php
+++ resources/views/admin/staff/staff.blade.php
@@ -3,6 +3,7 @@
     <div class="row">
         <div class="row">
             <div class="input-field col s12">
+                {{ $user }}
                 <select id="userselect">
                     <option value="" disabled selected>Choose your option</option>
                     @foreach($user as $i=>$value)
